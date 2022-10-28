f26a9084e97a819a58f4b1212ea7a6aff06bb518
Chewie <chewie@thecoon.net>
2022-10-28 21:27:51 +0200

    added java.config + temperature-data.txt + implementation of producer.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"
-                   [org.clojure/data.json "2.4.0"]
-                   [org.apache.kafka/kafka-clients "2.8.0"]]]
-  :repl-options {:init-ns kafka-prototype.core})
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.json "2.4.0"]
+                 [org.apache.kafka/kafka-clients "2.8.0"]
+                 [cheshire "5.11.0"]]
+  :repl-options {:init-ns kafka-prototype.core}
+  :aliases {"consumer" ["run" "-m" "kafka-prototype.consumer"]
+            "producer" ["run" "-m" "kafka-prototype.producer"]})
--- src/kafka_prototype/producer.clj
+++ src/kafka_prototype/producer.clj
@@ -1,1 +1,62 @@
-(ns kafka-prototype.producer)
+(ns kafka-prototype.producer
+  (:gen-class)
+  (:require [clojure.data.json :as json]
+            [clojure.java.io :as cio]
+            [cheshire.core :as cc-json])
+  (:import
+    (java.util Properties)
+    (org.apache.kafka.clients.admin AdminClient NewTopic)
+    (org.apache.kafka.clients.producer Callback KafkaProducer ProducerConfig ProducerRecord)
+    (org.apache.kafka.common.errors TopicExistsException)))
+
+
+(defn- set-properties [config-file]
+  (with-open [config (cio/reader config-file)]
+    (doto (Properties.)
+      (.putAll
+        {ProducerConfig/KEY_SERIALIZER_CLASS_CONFIG   "org.apache.kafka.common.serialization.StringSerializer"
+         ProducerConfig/VALUE_SERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringSerializer"})
+      (.load config))))
+
+(defn- create-topic! [topic partitions replication machine-config]
+  (println "called")
+  (let [admin-client (AdminClient/create machine-config)]
+    (try
+      (.createTopics admin-client [(NewTopic. ^String topic (int partitions) (short replication))])
+      (println "Topic created")
+      (catch TopicExistsException e
+        nil)
+      (finally
+        (.close admin-client)))))
+
+(defn producer! [config-file input-file]
+  (let [props (set-properties config-file)
+        print-ex       (comp println (partial str "Failed to deliver message: "))
+        print-metadata #(printf "Produced record to topic %s partition [%d] @ offest %d\n"
+                                (.topic %)
+                                (.partition %)
+                                (.offset %))
+        messages (mapv (fn [data]
+                         (let [td (cc-json/parse-string data)]
+                           td)) (line-seq
+                                  (let [r (cio/reader input-file)]
+                                    r)))]
+    (create-topic! "Sensor_1" 1 3 props)
+    (create-topic! "Sensor_2" 1 3 props)
+    (with-open [producer (KafkaProducer. props)]
+      (let [callback (reify Callback
+                         (onCompletion [this metadata exception]
+                           (if exception
+                             (print-ex exception)
+                             (print-metadata metadata))))]
+        #p(map (fn [td]
+                 (let [k  (first (keys td))
+                       v  (get td k)]
+                   (.send producer (ProducerRecord. k (cc-json/generate-string k) (cc-json/generate-string v)) callback))) messages)
+        (.flush producer)))))
+
+(defn -main [& args]
+  (apply producer! args))
+        
+        
+
