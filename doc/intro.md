aba3c5ff5b1265a7c2ba30720e4c22459c8308d8
Chewie <chewie@thecoon.net>
2022-10-28 16:53:29 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kafka-prototype
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kafka_prototype/core_test.clj
@@ -0,0 +1,7 @@
+(ns kafka-prototype.core-test
+  (:require [clojure.test :refer :all]
+            [kafka-prototype.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
